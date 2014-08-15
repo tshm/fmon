acebdaa33867bd1711f5386145d01dd3a036646e
Tosh Satake <2sm@csc.jp>
2014-08-15 10:07:31 +0100

    autocollect test targets

--- test.hs
+++ test.hs
@@ -1,5 +1,20 @@
 import Test.DocTest
+import System.FilePath
+import System.Directory
+
+excludeFiles :: [FilePath]
+excludeFiles = ["fmon.hs", "Test.hs", "Setup.hs"]
+
+-- |
+-- >>> collectTestFiles ["a.cpp","a.hs","c",".d","fmon.hs"]
+-- ["a.hs"]
+collectTestFiles :: [FilePath] -> [FilePath]
+collectTestFiles files = filter (\n -> isHs n && inc n) files where
+  isHs = (== ".hs") . snd . splitExtension
+  inc n = not $ elem n excludeFiles
 
 main :: IO ()
-main = doctest ["-isrc", "watch.hs", "optionparser.hs"]
+main = do
+  files <- fmap collectTestFiles $ getDirectoryContents "."
+  doctest $ "-isrc" : files
 
